298b9965994eb2abfa2e40fdb62d355f244b1819
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2014-10-04 10:44:43 +0100

    Added the most recent version of ALU.vhd
    That version was written by Rafael.

--- proj1/ALU.vhd
+++ proj1/ALU.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date:    11:49:39 10/03/2014 
+-- Create Date:    00:59:05 10/04/2014 
 -- Design Name: 
--- Module Name:    ALU - Behavioral 
+-- Module Name:    alu - Behavioral 
 -- Project Name: 
 -- Target Devices: 
 -- Tool versions: 
@@ -22,49 +22,67 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_SIGNED.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity ALU is
-    Port ( a : in  STD_LOGIC_VECTOR (6 downto 0);
-           b : in  STD_LOGIC_VECTOR (12 downto 0);
-			  inst: in STD_LOGIC_VECTOR (2 downto 0);
-			  clk : in STD_LOGIC;
-           res : out  STD_LOGIC_VECTOR (12 downto 0));
-end ALU;
+entity alu is
+	Port ( a : in  STD_LOGIC_VECTOR (6 downto 0);
+		b : in  STD_LOGIC_VECTOR (12 downto 0);
+		inst : in  STD_LOGIC_VECTOR (2 downto 0);
+		res : out  STD_LOGIC_VECTOR (12 downto 0));
+end alu;
 
-architecture Behavioral of ALU is
-	signal oper1,oper2: STD_LOGIC_VECTOR (12 downto 0);
-	signal product: STD_LOGIC_VECTOR (25 downto 0);
-	signal shift: STD_LOGIC_VECTOR (12 downto 0);
-	signal preres: STD_LOGIC_VECTOR (12 downto 0);
+architecture Behavioral of alu is
+	signal ct_a : STD_LOGIC_VECTOR (12 downto 0);
+	signal ct_b : STD_LOGIC_VECTOR (12 downto 0);
+	signal sum : STD_LOGIC_VECTOR (12 downto 0);
+	signal product : STD_LOGIC_VECTOR (25 downto 0);
+	signal short_product : STD_LOGIC_VECTOR (12 downto 0);
+	signal shift : STD_LOGIC_VECTOR (12 downto 0);
+	signal nored : STD_LOGIC_VECTOR (12 downto 0);
+	signal mux_a : STD_LOGIC_VECTOR (12 downto 0);
+	signal mux_b : STD_LOGIC_VECTOR (12 downto 0);
+	signal sm_mux_a : STD_LOGIC_VECTOR (12 downto 0);
 begin
-	oper1 <= "0000000" & a(5 downto 0) when a(6)='0' else not ("0000000" & a(5 downto 0)) + 1;
-	oper2 <= "0" & b(11 downto 0) when b(12)='0' else not ("0" & b(11 downto 0)) + 1;
-	product <= oper1 * oper2;
-	with oper1(2 downto 0) select
-		shift <= oper2 when "000",
-				oper2(12) & oper2(12 downto 1) when "001",
-				oper2(12) & oper2(12) & oper2(12 downto 2) when "010",
-				oper2(12) & oper2(12) & oper2(12) & oper2(12 downto 3) when "011",
-				oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12 downto 4) when "100",
-				oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12 downto 5) when "101",
-				oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12 downto 6) when "110",
-				oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12) & oper2(12 downto 7) when others;
-	with inst select
-		preres <= oper1 + oper2								when "000", -- SUM
-				oper1 - oper2									when "001", -- SUB
-				product(25) & product(11 downto 0)		when "010", -- MUL
-				oper1 nor oper2								when "011", -- NOR
-				shift												when others; -- SHIFT
+
+
+	-- Conversion from signal-module notation to complement two:
+	ct_a <= "000000" & a(6 downto 0) when a(6)='0' else (not ("0000000" & a(5 downto 0)) + 1);
+	ct_b <= b(12 downto 0) when b(12)='0'else (not ("0" & b(11 downto 0)) + 1);
 	
-	res <= "0" & preres(11 downto 0) when preres(12)='0' else not("0" & preres(11 downto 0)) + 1;
-
+	
+	-- Results of the various functional units:
+	sum <= ct_b + ct_a when inst(0)='0' else ct_b - ct_a;
+	product <= ct_b * ct_a;
+	short_product <= product(25) & product(11 downto 0);
+		-- Shift Right Arithmetic:
+		-- The characteristic of a shift right arithmetic is the
+		-- conservation of the signal, hence our option to keep that bit
+		with a(2 downto 0) select
+			shift <= b when "000",
+				b(12) & "0" & b(11 downto 1) when "001",
+				b(12) & "00" & b(11 downto 2) when "010",
+				b(12) & "000" & b(11 downto 3) when "011",
+				b(12) & "0000" & b(11 downto 4) when "100",
+				b(12) & "00000" & b(11 downto 5) when "101",
+				b(12) & "000000" & b(11 downto 6) when "110",
+				b(12) & "0000000" & b(11 downto 7) when others;
+			
+		-- On the NOR gate, we chose to keep the signal bit in the
+		-- beginning as well, for the sake of coherence
+	nored <= (a(6) & "000000" & a(5 downto 0)) nor b(12 downto 0);
+	
+	
+	-- First multiplexers:
+	mux_a <= sum when inst(1)='0' else short_product;
+	mux_b <= shift when inst(1)='0' else nored;
+	
+	
+	-- Reconversion to signal-module notation of the adder/multiplier result
+	sm_mux_a <= mux_a when mux_a(12)='0' else (not("0" & mux_a(11 downto 0)) + 1);
+	
+	-- Last multiplexer;
+	res <= sm_mux_a when inst(2)='0' else mux_b;
 end Behavioral;
-
