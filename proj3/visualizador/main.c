cea9a13e965f92843d8d837a9ae27c23bc920b43
raffitz <raf.a.m.c.gon@gmail.com>
2014-11-22 00:14:30 +0000

    Added the framework for the visualizer

---
+++ proj3/visualizador/main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+
+
+int main(int argc, char** argv)
+{
+	
+}
