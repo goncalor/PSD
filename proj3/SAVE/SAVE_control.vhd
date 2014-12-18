479850db5631d7a7c7e6c4b4d90416c18c05e508
raffitz <raf.a.m.c.gon@gmail.com>
2014-12-18 18:29:12 +0000

    Added content to the SAVE FSM, though still incomplete

--- proj3/SAVE/SAVE_control.vhd
+++ proj3/SAVE/SAVE_control.vhd
@@ -32,6 +32,7 @@ entity SAVE_control is
 		start : in  STD_LOGIC;
 		clk : in  STD_LOGIC;
 		rst : in  STD_LOGIC;
+		final : in STD_LOGIC;
 		ww : in STD_LOGIC_VECTOR (1 downto 0);
 		op_type : in STD_LOGIC_VECTOR (2 downto 0);
 		
@@ -45,13 +46,13 @@ entity SAVE_control is
 		cs : out STD_LOGIC; -- Composite block input select
 		ofs : out STD_LOGIC; -- Output function select
 		os : out STD_LOGIC; -- Output select
-		o_rst : out STD_LOGIC; -- Output Memory Address Manager reset
-		o_offset : out STD_LOGIC_VECTOR(2 downto 0)); -- Output Memory AddMan offset
+		o_en : out STD_LOGIC; -- Write enable for output RAM
+		o_rst : out STD_LOGIC; -- Output Memory Address Manager reset offset
 end SAVE_control;
 
 architecture Behavioral of SAVE_control is
 	--Use descriptive names for the states, like st1_reset, st2_search
-	type state_type is (st1_<name_state>, st2_<name_state>, ...); 
+	type state_type is (sm_idle, first_line, normal, sm_stop); 
 	signal state, next_state : state_type; 
 begin
 
@@ -59,7 +60,7 @@ SYNC_PROC: process (clk)
 	begin
 		if (clk'event and clk = '1') then
 			if (rst = '1') then
-				state <= st1_<name_state>;
+				state <= sm_idle;
 			else
 				state <= next_state;
 			end if;        
@@ -69,16 +70,137 @@ SYNC_PROC: process (clk)
 	--MOORE State-Machine - Outputs based on state only
 	OUTPUT_DECODE: process (state)
 	begin
-		--insert statements to decode internal output signals
-		--below is simple example
-		if state = st3_<name> then
-			<output>_i <= '1';
-		else
-			<output>_i <= '0';
-		end if;
+		case (state) is
+			when sm_idle =>
+				orw_old <= 'X';
+				m_ort <= 'X';
+				c_ort <= 'X';
+				e_is <= 'X';
+				d_is <= 'X';
+				cfs <= 'X';
+				cs <= 'X';
+				ofs <= 'X';
+				os <= 'X';
+				o_en <= '0';
+				o_rst <= '1';
+			when first_line =>
+				orw_old <= '1';
+				case (op_type) is
+					when "000" => -- Erosion
+						m_ort <= '1';
+						c_ort <= 'X';
+						e_is <= '0';
+						d_is <= 'X';
+						cfs <= 'X';
+						cs <= 'X';
+						ofs <= '0';
+						os <= '0';
+					when "001" => -- Dilation
+						m_ort <= '0';
+						c_ort <= 'X';
+						e_is <= 'X';
+						d_is <= '0';
+						cfs <= 'X';
+						cs <= 'X';
+						ofs <= '1';
+						os <= '0';
+					when "010" => -- E + D
+						m_ort <= '1';
+						c_ort <= '0';
+						e_is <= '0';
+						d_is <= '1';
+						cfs <= '0';
+						cs <= '1';
+						ofs <= '1';
+						os <= '0';
+					when "011" => -- D+E
+						m_ort <= '0';
+						c_ort <= '1';
+						e_is <= '1';
+						d_is <= '0';
+						cfs <= '1';
+						cs <= '1';
+						ofs <= '0';
+						os <= '0';
+					when others => -- Edge find
+						m_ort <= '1';
+						c_ort <= 'X';
+						e_is <= '0';
+						d_is <= 'X';
+						cfs <= 'x';
+						cs <= '0';
+						ofs <= 'X';
+						os <= '1';
+				end case;
+				o_en <= '1';
+				o_rst <= '0';
+			when normal =>
+				orw_old <= '0';
+				case (op_type) is
+					when "000" => -- Erosion
+						m_ort <= '1';
+						c_ort <= 'X';
+						e_is <= '0';
+						d_is <= 'X';
+						cfs <= 'X';
+						cs <= 'X';
+						ofs <= '0';
+						os <= '0';
+					when "001" => -- Dilation
+						m_ort <= '0';
+						c_ort <= 'X';
+						e_is <= 'X';
+						d_is <= '0';
+						cfs <= 'X';
+						cs <= 'X';
+						ofs <= '1';
+						os <= '0';
+					when "010" => -- E + D
+						m_ort <= '1';
+						c_ort <= '0';
+						e_is <= '0';
+						d_is <= '1';
+						cfs <= '0';
+						cs <= '1';
+						ofs <= '1';
+						os <= '0';
+					when "011" => -- D+E
+						m_ort <= '0';
+						c_ort <= '1';
+						e_is <= '1';
+						d_is <= '0';
+						cfs <= '1';
+						cs <= '1';
+						ofs <= '0';
+						os <= '0';
+					when others => -- Edge find
+						m_ort <= '1';
+						c_ort <= 'X';
+						e_is <= '0';
+						d_is <= 'X';
+						cfs <= 'x';
+						cs <= '0';
+						ofs <= 'X';
+						os <= '1';
+				end case;
+				o_en <= '1';
+				o_rst <= '0';
+			when others =>
+				orw_old <= 'X';
+				m_ort <= 'X';
+				c_ort <= 'X';
+				e_is <= 'X';
+				d_is <= 'X';
+				cfs <= 'X';
+				cs <= 'X';
+				ofs <= 'X';
+				os <= 'X';
+				o_en <= '0';
+				o_rst <= '0';
+		end case;
 	end process;
 
-	NEXT_STATE_DECODE: process (state, <input1>, <input2>, ...)
+	NEXT_STATE_DECODE: process (state, start, stop)
 	begin
 	--declare default state for next_state to avoid latches
 		next_state <= state;  --default is to stay in current state
